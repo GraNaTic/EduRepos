4b627a4591408921adf06ad8c55e54aaee7bafe0
Grisha <greg.baranov@mail.ru>
2023-02-15 14:16:46 +0300

    Initial commit.

---
+++ FrontScreen/Monster.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontScreen
+{
+    abstract class Monster
+    {
+        private int x;
+
+        private int y;
+
+        private int weight;
+
+        public Monster (int x, int y)
+        {
+            Random random = new Random();
+            int weight = random.Next (0, 75);
+            BeBorn(x, y, weight);
+        }
+
+        public void X(int arg)
+        {
+            x = arg;
+        }
+
+        public int X()
+        {
+            return x;
+        }
+
+        public void Y(int arg)
+        {
+            y = arg;
+        }
+
+        public int Y()
+        {
+            return y;
+        }
+
+        public void Weight(int arg)
+        {
+            weight = arg;
+        }
+
+        public int Weight()
+        {
+            return weight;
+        }
+
+        public void BeBorn( int x, int y, int weight)
+        {
+            X(x);
+            Y(y);
+            Weight(weight);
+        }
+    }
+
+    class Dog: Monster
+    {
+        private ConsoleColor clr;
+
+        private char face;
+
+        public Dog ( int x, int y):base (x, y)
+        {
+            Face('@');
+            if (Weight() > 55)
+                Color(ConsoleColor.Magenta);
+            else
+                if (Weight() - 27>0)
+                    Color(ConsoleColor.Yellow);
+                else 
+                    Color(ConsoleColor.DarkYellow);
+        }
+
+        public ConsoleColor Color()
+        {
+            return clr;
+        }
+
+        public void Color(ConsoleColor color)
+        {
+            clr = color;
+        }
+
+        public char Face()
+        {
+            return face;
+        }
+
+        public void Face(char arg)
+        {
+            face = arg;
+        }
+        
+        public static Dog GetObj()
+        {
+            Random random = new Random();
+            var dog = new Dog(random.Next(120), random.Next(1, 30));
+            return dog;
+        }
+    }
+
+    class Hedgehog : Dog
+    {
+        public Hedgehog(int x, int y) : base(x, y)
+        {
+            Face('Ж');
+            Color(ConsoleColor.Gray);
+        }
+        new public static Hedgehog GetObj()
+        {
+            Random random = new Random();
+            var hedgehog = new Hedgehog(random.Next(120), random.Next(1, 30));
+            return hedgehog;
+        }
+    }
+}
---
+++ FrontScreen/Program.cs
@@ -0,0 +1,55 @@
+﻿using FrontScreen;
+
+class Program
+{
+    public static void Main()
+    {
+        
+        for (int i = 0; i < 150; i++)
+        {                        
+            TypeDogOrHedg(Dog.GetObj());
+            TypeDogOrHedg(Hedgehog.GetObj());            
+        }
+        Console.SetCursorPosition(0, 31);
+        Console.Clear();
+        TypeListDogs();
+    }
+    public static void TypeDogOrHedg(Dog dog)
+    {
+        Console.SetCursorPosition(dog.X(), dog.Y());
+        Console.ForegroundColor = dog.Color();
+        Console.Write(dog.Face());
+        //Thread.Sleep(1);
+    }
+
+
+    public static void TypeListDogs()
+    {
+        Random random = new Random();
+        List <Dog> dogs = new List <Dog>();
+        for (int i = 0; i < 75; i++)
+        {
+            dogs.Add(Dog.GetObj());
+        }
+        while (true)
+        {            
+            foreach (var dog in dogs)
+            {
+                Console.SetCursorPosition(dog.X(), dog.Y());
+                //Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(" ");                
+                if (dog.Y() + 1 < 31)
+                    dog.Y(dog.Y() + 1);
+                else
+                {
+                    dog.Y(1);
+                    dog.X(random.Next(120));
+                }
+                Console.SetCursorPosition(dog.X(), dog.Y());
+                //Console.BackgroundColor = ConsoleColor.DarkRed;
+                TypeDogOrHedg(dog);                
+            }
+            Thread.Sleep(100);
+        }
+    }
+}
